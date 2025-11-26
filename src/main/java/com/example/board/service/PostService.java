cf1999ece9b5ee016b7a59035208fed11db70c42
5chang2 <och8808@gmail.com>
2025-11-26 14:16:14 +0900

    N+1 문제 해결(FETCH/EntityGraph)

--- src/main/java/com/example/board/service/PostService.java
+++ src/main/java/com/example/board/service/PostService.java
@@ -133,6 +133,15 @@ public class PostService {
         return postRepository.findAllBy(pageable);
     }
 
+    public List<Post> getAllPostsWithFetchJoin() {
+//        return postRepository.findAll();
+        return postRepository.findAllWithComments();
+    }
+
+    public List<Post> getAllPostsWithEntityGraph() {
+        return postRepository.findAllWithCommentsEntityGraph();
+    }
+
 }
 
 
