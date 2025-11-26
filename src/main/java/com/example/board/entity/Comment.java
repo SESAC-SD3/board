e7d3fa15c11c8ca0c234c23e6d9fed0e4ef1610f
5chang2 <och8808@gmail.com>
2025-11-26 11:22:01 +0900

    편의메서드

--- src/main/java/com/example/board/entity/Comment.java
+++ src/main/java/com/example/board/entity/Comment.java
@@ -34,8 +34,13 @@ public class Comment {
 
     public Comment(String content, Post post) {
         this.content = content;
-        this.post = post;
+//        this.post = post;
+        if (post != null) {
+            post.addComment(this);
+        }
     }
 
 
 }
+
+
